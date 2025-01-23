a7b81917aee0df7e3e1bf9deaf2727ae4c40397e
ReynardL <163877889+ReynardL@users.noreply.github.com>
2025-01-23 13:24:32 -0500

    Code and structure response fixes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime
 from typing import Dict, Optional
 
@@ -11,35 +11,38 @@ i = 0
 class Task(BaseModel):
     title: str
     description: str
-    start: Optional[datetime] = datetime.now()
-    # due: datetime
     completed: Optional[bool] = False 
+    created: datetime = Field(default_factory=datetime.now)
+
+class TaskResponse(BaseModel):
+    message: str
+    task: Task
 
 @app.get("/tasks", response_model=Dict[int, Task])
 def get_tasks():
     return tasks
 
-@app.post("/tasks")
+@app.post("/tasks", response_model=TaskResponse)
 def create_task(task: Task):
     global i
     tasks[i] = task
     i += 1
     return {"message": "Task Created Succesfully", "task": task}
 
-@app.get("/tasks/{id}", response_model = Task)
+@app.get("/tasks/{id}", response_model=Task)
 def get_task_by_id(id: int):
     if id not in tasks: 
         raise HTTPException(status_code=404, detail="Task not found")
     return tasks[id]
 
-@app.put("/tasks/{id}")
-def update_task(id: int, new_task = Task):
+@app.put("/tasks/{id}", response_model=TaskResponse)
+def update_task(id: int, new_task: Task):
     if id not in tasks: 
         raise HTTPException(status_code=404, detail="Task not found")
     tasks[id] = new_task
-    return {"message": "Taskcd Updated Succesfully", "task": new_task}
+    return {"message": "Task Updated Succesfully", "task": new_task}
 
-@app.delete("/tasks/{id}")
+@app.delete("/tasks/{id}", response_model=Dict[str, str])
 def delete_task(id: int):
     if id not in tasks: 
         raise HTTPException(status_code=404, detail="Task not found")
