122059ae71224a823de7b96ca204ba6fe38b1557
ReynardL <163877889+ReynardL@users.noreply.github.com>
2025-01-27 15:06:44 -0500

    Deployment Fix

--- frontend/Webpack.config.js
+++ frontend/Webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
@@ -33,7 +32,6 @@ module.exports = {
     },
     plugins: [
         new Dotenv(),
-        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: "./public/index.html",
         }),
